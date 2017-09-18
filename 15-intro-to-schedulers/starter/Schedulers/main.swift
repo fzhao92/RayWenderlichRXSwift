f49a71931e7ec343011e9c709375201620186ba0
Forrest Zhao <fozhao92@gmail.com>
2017-09-17 21:06:54 -0700

    wip: ch 15

--- 15-intro-to-schedulers/starter/Schedulers/main.swift
+++ 15-intro-to-schedulers/starter/Schedulers/main.swift
@@ -31,6 +31,41 @@ let animal = BehaviorSubject(value: "[dog]")
 
 
 animal
+  .subscribeOn(MainScheduler.instance)
   .dump()
+  .observeOn(globalScheduler)
   .dumpingSubscription()
   .disposed(by: bag)
+
+let fruit = Observable<String>.create { (observer) in
+  observer.onNext("[apple]")
+  sleep(2)
+  observer.onNext("[pineapple]")
+  sleep(2)
+  observer.onNext("[strawberry]")
+  return Disposables.create()
+}
+
+let animalsThread = Thread() {
+  sleep(3)
+  animal.onNext("[cat]")
+  sleep(3)
+  animal.onNext("[tiger]")
+  sleep(3)
+  animal.onNext("[fox]")
+  sleep(3)
+  animal.onNext("[leopard]")
+}
+
+animalsThread.name = "Animals Thread"
+animalsThread.start()
+
+fruit
+  .subscribeOn(globalScheduler)
+  .dump()
+  .observeOn(MainScheduler.instance)
+  .dumpingSubscription()
+  .disposed(by: bag)
+
+RunLoop.main.run(until: Date(timeIntervalSinceNow: 13))
+
